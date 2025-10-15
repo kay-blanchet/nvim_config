93edcf13f80ee6caa8b94bbce45b975149fb01f9
kblanche <kblanche@student.42angouleme.fr>
2025-10-15 19:04:37 +0200

    adding a theme and snacks

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -34,7 +34,7 @@ return {
 					lsp.lua_ls.setup {
 						capabilities = capabilities,
 						settings = {
-							Lua = {
+							lua = {
 								runtime = { version = "Lua 5.1" },
 								diagnostics = {
 									globals = { "vim", "it", "describe", "before_each", "after_each" },
